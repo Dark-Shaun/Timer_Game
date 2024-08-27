40123ef8dd680c0e935651a8a0bb38f0384fc20d
Dark-Shaun <63874836+Dark-Shaun@users.noreply.github.com>
2024-08-27 12:18:29 -0700

    Update ResultModal.jsx and Added comments for more detailed explanation of the code

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,7 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
-
+// utilizing the forwardRef whihc will aloow me to move the refs to the children that is from resultmodal to the Time challengers
+// Referred this documentation- (https://react.dev/reference/react/forwardRef)
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, remainingTime, onReset },
   ref
@@ -10,7 +11,8 @@ const ResultModal = forwardRef(function ResultModal(
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
-
+  // use the below hook to customize the handle exposed as a ref
+  // referred to this documentation- (https://react.dev/reference/react/useImperativeHandle)
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -18,7 +20,8 @@ const ResultModal = forwardRef(function ResultModal(
       },
     };
   });
-
+  // return create portal utilized the createPortal in React API to make sure this can be used for dialogue to appear
+  // Check the createPortal here (https://react.dev/reference/react-dom/createPortal)
   return createPortal(
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You lost</h2>}
